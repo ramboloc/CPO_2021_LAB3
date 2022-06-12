4cd1e4b3de234fa7aa2db3e770fe9308e32e763b
ramboloc <1255985865@qq.com>
2022-06-12 13:01:38 +0800

    update code

--- mathExpCal.py
+++ mathExpCal.py
@@ -1,4 +1,5 @@
 from math import *
+import os
 
 # Operation level
 operators = ['+', '-', '*', '/', '(', ')', ',']
@@ -10,19 +11,24 @@ def negative_test(f):
         try:
             return f(*args, **kwargs)
         except ValueError:
-            print('Wrong Expression! Please check the expression!')
+            print('Error! Please input right expression!')
 
     return test
+
+
+def dot_to_png(num):
+    command = 'dot pic%d.dot -T png -o pic%d.png' % (num, num)
+    os.system(command)
 
 
 class MathExpression(object):
     def __init__(self, formula='0'):
         self.formula = formula  # origin formula
-        self.conv_form = []  # Different versions of formulas
+        self.postfix_form = []  # Different versions of formulas
         self.values = dict()
 
-    def RPN(self):
-        """ convert origin formula to RPN"""
+    def to_Postfix(self):
+        """ convert origin formula to Postfix Expression"""
         # clean the blank
         ori_form = self.formula.replace(' ', '')
 
@@ -35,14 +41,13 @@ class MathExpression(object):
 
         for index, i in enumerate(ori_form):
             # 1. int float   2.str include operators  3.seperator:, ()
-
-            # 1 pop the intergers and float into stack
+            # 1 pop the integers and float into stack
             if ((i >= '0') and (i <= '9')) or i == '.':
                 if flag_long_num == 0:
-                    self.conv_form.append(i)
+                    self.postfix_form.append(i)
                     flag_long_num = 1
                 else:
-                    self.conv_form[-1] = self.conv_form[-1] + i
+                    self.postfix_form[-1] = self.postfix_form[-1] + i
                 continue
 
             flag_long_num = 0
@@ -57,7 +62,7 @@ class MathExpression(object):
                 # '+', '-', '*', '/'
                 else:
                     if len(long_str) == 1:
-                        self.conv_form.append(long_str)
+                        self.postfix_form.append(long_str)
                     else:
                         op_stack.append(long_str)
                     flag_long_str = 0
@@ -82,30 +87,31 @@ class MathExpression(object):
             if i == ')':
                 # The expressions in brackets operate first, and the () is not needed in the conv_form
                 while op_stack[-1] != '(':
-                    self.conv_form.append(op_stack.pop(-1))
+                    self.postfix_form.append(op_stack.pop(-1))
                 op_stack.pop(-1)
                 # pop special operators like sin cos func
                 if (len(op_stack) != 0) and (op_stack[-1] not in operators):
-                    self.conv_form.append(op_stack.pop(-1))
+                    self.postfix_form.append(op_stack.pop(-1))
                 continue
 
             # continue the str operation, the '+', '-', '*', '/', Identified, but not yet used
             while len(op_stack) != 0 and op_levels[op_stack[-1]] >= op_levels[i]:
-                self.conv_form.append(op_stack.pop(-1))
+                self.postfix_form.append(op_stack.pop(-1))
             op_stack.append(i)
 
         if flag_long_str == 1:
-            self.conv_form.append(long_str)
+            self.postfix_form.append(long_str)
 
         while len(op_stack) != 0:
-            self.conv_form.append(op_stack.pop(-1))
+            self.postfix_form.append(op_stack.pop(-1))
 
     @negative_test
     def calc(self, **kwargs):
+        """ Calculation formula, simulating trees with stacks """
         stack = list()
         self.values = kwargs
         # e.g.:（3+4）*5-6 -> 34+5*6-
-        for i in self.conv_form:
+        for i in self.postfix_form:
             if i == 'sin':
                 stack.append(sin(stack.pop(-1)))
             elif i == 'cos':
@@ -160,38 +166,39 @@ class MathExpression(object):
         res = list()
         res.append('digraph G {')
         res.append(' rankdir=BT;')
+        for i, n in enumerate(self.postfix_form):
+            res.append(' n{}[label="{}"];'.format(i, n))
 
-        for i, n in enumerate(self.conv_form):
-            res.append(' n_{}[label="{}"];'.format(i, n))
-
-        index = len(self.conv_form)
+        index = len(self.postfix_form)
 
         stack = list()
-        for i, n in enumerate(self.conv_form):
+        for i, n in enumerate(self.postfix_form):
             if n in ['sin', 'cos', 'tan']:
                 cur = stack.pop(-1)
-                res.append('{} -> n_{};'.format(cur, i))
-                stack.append('n_{}'.format(i))
+                res.append('{} -> n{};'.format(cur, i))
+                stack.append('n{}'.format(i))
             elif n in operators or i in ['log', 'pow']:
                 cur1 = stack.pop(-1)
                 cur2 = stack.pop(-1)
-                res.append('{} -> n_{};'.format(cur1, i))
-                res.append('{} -> n_{};'.format(cur2, i))
-                stack.append('n_{}'.format(i))
+                res.append('{} -> n{};'.format(cur1, i))
+                res.append('{} -> n{};'.format(cur2, i))
+                stack.append('n{}'.format(i))
             elif n not in self.values.keys():
-                stack.append('n_{}'.format(i))
+                stack.append('n{}'.format(i))
             elif len(n) == 1:
-                res.append(' n_{}[label="{}"];'.format(index, self.values[n]))
-                res.append('n_{} -> n_{};'.format(index, i))
+                res.append(' n{}[label="{}"];'.format(index, self.values[n]))
+                res.append('n{} -> n{};'.format(index, i))
                 index += 1
-                stack.append('n_{}'.format(i))
+                stack.append('n{}'.format(i))
             else:
                 f = self.values[n]
                 args_nums = f.__code__.co_argcount
                 for j in range(args_nums):
                     cur = stack.pop(-1)
-                    res.append('{} -> n_{};'.format(cur, i))
-                stack.append('n_{}'.format(i))
+                    res.append('{} -> n{};'.format(cur, i))
+                stack.append('n{}'.format(i))
         res.append('}')
         file = open('pic%d.dot' % num, 'w')
         file.write("\n".join(res))
+        file.close()
+        dot_to_png(num)
--- mathetest.py
+++ mathetest.py
@@ -1,69 +1,68 @@
 import unittest
 from mathExpCal import *
-from graphviz import Digraph
 
 
 class MathExpressionTest(unittest.TestCase):
-    def test_sincostan(self):
+    def test_sin_cos_tan(self):
         i = MathExpression('cos(0)+tan(0.3)*sin(0)')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(1)
-        self.assertEqual(i.calc(), 1.0)
+        self.assertEqual(i.calc(), cos(0)+tan(0.3)*sin(0))
 
     def test_add(self):
         i = MathExpression('22.2+33.3+44.4')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(2)
         self.assertEqual(i.calc(), 99.9)
 
     def test_minus(self):
         i = MathExpression('10-(7-2)')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(3)
         self.assertEqual(i.calc(), 5.0)
         i = MathExpression('19-8-4-2')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(4)
         self.assertEqual(i.calc(), 5.0)
 
     def test_mul(self):
         i = MathExpression('19-14.21*20.91')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(5)
         self.assertEqual(i.calc(), -278.1311)
         i = MathExpression('(8-12)*func(10)')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(6)
         self.assertEqual(i.calc(func=lambda x: x * x), -400.0)
 
     def test_dev(self):
         i = MathExpression('77/6')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(7)
         self.assertEqual(round(i.calc(), 2), 12.83)
 
-    def test_powlog(self):
+    def test_pow_log(self):
         i = MathExpression('pow(2,2)*log(9,3)')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(8)
         self.assertEqual(i.calc(), 8.0)
 
     def test_specialFunc(self):
         i = MathExpression('cos(0)+func(a)')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(9)
         self.assertEqual(i.calc(a=2, func=lambda x: x * 2), 5.0)
 
     # hard test
     def test_1(self):
         i = MathExpression('a + 2 - sin(0.3)*(b - c)')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(10)
         self.assertEqual(round(i.calc(a=1.1, b=77, c=66), 2), -0.15)
 
     def test_2(self):
         i = MathExpression('cos((3-a)*b)+func(c,d)/3')
-        i.RPN()
+        i.to_Postfix()
         i.visualize(11)
         self.assertEqual(i.calc(a=3, b=2, c=20, d=10, func=lambda x, y: x + y), 11.0)
 
